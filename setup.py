86c50cd6e20c069c726fad1f9e2775f9e387156c
Mehak-Munjal <mehakmunjal00@gmail.com>
2020-11-16 12:13:19 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="TOPSIS_Mehak_101983050",
+version="0.1",
+description="Assignment06",
+url="https://github.com/Mehak-Munjal/TOPSIS",
+author="Mehak",
+packages=['TOPSIS_Mehak_101983050'],
+install_requires=['pandas'])
